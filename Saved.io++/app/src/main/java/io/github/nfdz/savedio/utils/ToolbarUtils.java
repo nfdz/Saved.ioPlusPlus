df6c64506d4011fde4622004a3c7ac58686c817c
nfdz <noe.fernandez@protonmail.com>
2017-04-05 02:37:28 +0200

    remove unused android tests

--- Saved.io++/app/src/main/java/io/github/nfdz/savedio/utils/ToolbarUtils.java
+++ Saved.io++/app/src/main/java/io/github/nfdz/savedio/utils/ToolbarUtils.java
@@ -4,24 +4,7 @@
 package io.github.nfdz.savedio.utils;
 
 
-import android.media.Image;
-import android.os.AsyncTask;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
-
-import io.github.nfdz.savedio.R;
-
-import static android.support.v7.appcompat.R.attr.logo;
 
 /**
  * This class contains static methods to ease work with application toolbar.
