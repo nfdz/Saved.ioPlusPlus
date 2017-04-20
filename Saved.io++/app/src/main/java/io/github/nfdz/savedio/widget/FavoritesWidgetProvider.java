7dda1258addb51bbd3567a476e58b4bbdfac269a
nfdz <noe.fernandez@protonmail.com>
2017-04-20 16:52:27 +0200

    testing dummy widget

---
+++ Saved.io++/app/src/main/java/io/github/nfdz/savedio/widget/FavoritesWidgetProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 Noe Fernandez
+ */
+package io.github.nfdz.savedio.widget;
+
+import android.annotation.TargetApi;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import io.github.nfdz.savedio.R;
+
+public class FavoritesWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_icon, description);
+    }
+}
